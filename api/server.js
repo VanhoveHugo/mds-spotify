2b3b0a69bbccc58bff2e33d8e50b372ba3986c0e
VanhoveHugo <vanhovehugo1@gmail.com>
2024-01-28 20:52:07 +0100

    Users: Swagger

--- api/server.js
+++ api/server.js
@@ -1,13 +1,43 @@
 const express       = require('express');
 const mongoose      = require('mongoose');
+const swaggerUi     = require('swagger-ui-express');
+const swaggerJSDoc  = require('swagger-jsdoc');
 
 const app           = express();
 const port          = process.env.API_PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://admin:admin@cluster0.snpqq.mongodb.net/spotify?retryWrites=true")
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Documentation API',
+            version: '1.0.0',
+        },
+        components: {
+            securitySchemes: {
+                Authorization: {
+                    type: "apiKey",
+                    in: "header",
+                    name: "Authorization",
+                },
+            },
+        },
+        security: [
+            {
+                Authorization: [],
+            },
+        ],
+    },
+    apis: ['./src/routes/*.js'],
+};
+
+const openAiSpec    = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openAiSpec));
 
 const userRoute     = require("./src/routes/userRoute");
 userRoute(app);
